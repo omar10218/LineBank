eadaef9b21f54d86607ef775ad2c8bfeb5e607c0
omar10218 <omar10218@gmail.com>
2021-07-23 09:05:51 +0800

    321

---
+++ src/app/f01001/f01001scn12/f01001scn12add/f01001scn12add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f01001scn12add',
+  templateUrl: './f01001scn12add.component.html',
+  styleUrls: ['./f01001scn12add.component.css']
+})
+export class F01001scn12addComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
