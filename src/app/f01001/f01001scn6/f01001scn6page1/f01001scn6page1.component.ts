823fb1d634f063bcada5f9271812b7b4f5c16559
omar10218 <omar10218@gmail.com>
2021-07-26 22:02:54 +0800

    error

--- src/app/f01001/f01001scn6/f01001scn6page1/f01001scn6page1.component.ts
+++ src/app/f01001/f01001scn6/f01001scn6page1/f01001scn6page1.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-f01001scn6page1',
   templateUrl: './f01001scn6page1.component.html',
   styleUrls: ['./f01001scn6page1.component.css','../../../../assets/css/f01.css']
 })
-export class F01001scn6page1Component implements OnInit {
+export class F01001scn6page1Component implements OnInit, AfterViewInit {
 
+  currentPage: PageEvent;
+  
   constructor() { }
 
   ngOnInit(): void {
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null
+    };
+    this.AAS003Source = null;
   }
 
+  totalCount: any;
+  @ViewChild('paginator', { static: true }) paginator: MatPaginator;
+  @ViewChild('sortTable', { static: true }) sortTable: MatSort;
+  AAS003Source = new MatTableDataSource<any>();
+
+  ngAfterViewInit() {
+    this.getAAS003S();
+    this.paginator.page.subscribe((page: PageEvent) => {
+      this.currentPage = page;
+      this.getAAS003S();
+    });
+  }
+
+  getAAS003S() {
+    this.totalCount = 10;
+  }
 }
