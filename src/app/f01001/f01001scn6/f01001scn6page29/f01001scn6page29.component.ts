318d5c37aa06d3210cad2bc4be12389a3a84d773
iphone100s <gp17457446@yahoo.com.tw>
2021-07-27 18:58:51 +0800

    前端TABLE

--- src/app/f01001/f01001scn6/f01001scn6page29/f01001scn6page29.component.ts
+++ src/app/f01001/f01001scn6/f01001scn6page29/f01001scn6page29.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { F01001scn6Service } from './../f01001scn6.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-f01001scn6page29',
@@ -7,9 +14,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class F01001scn6page29Component implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private f01001scn6Service: F01001scn6Service) { }
+  private applno: string;
+  private cuid: string;
+  currentPage: PageEvent;
+  currentSort: Sort;
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.applno = params['applno'];
+      this.cuid = params['cuid'];
+    });
+
+    this.currentPage = {
+      pageIndex: 0,
+      pageSize: 10,
+      length: null
+    };
+
+    this.currentSort = {
+      active: '',
+      direction: ''
+    };
+  }
+
+  totalCount: any;
+  @ViewChild('paginator', { static: true }) paginator: MatPaginator;
+  @ViewChild('sortTable', { static: true }) sortTable: MatSort;
+  KRM046Source = new MatTableDataSource<any>();
+
+  ngAfterViewInit() {
+    this.getKRM046();
+    this.paginator.page.subscribe((page: PageEvent) => {
+      this.currentPage = page;
+      this.getKRM046();
+    });
+  }
+
+  getKRM046() {
+    console.log("案件編號="+this.applno);
+    console.log("代碼=KRM046");
+    console.log("ID="+this.cuid);
+    const formdata: FormData = new FormData();
+    formdata.append('applno', this.applno);
+    formdata.append('cuid', this.cuid);
+    formdata.append('code', 'KRM046');
+    //queryDate之後從父模組來，目前先寫死(父模組日期取抓資料庫匯入下拉選單)
+    formdata.append('queryDate', '20210109');
+    formdata.append('page', `${this.currentPage.pageIndex + 1}`);
+    formdata.append('per_page', `${this.currentPage.pageSize}`);
+    this.f01001scn6Service.getJCICSearch(formdata).subscribe(data => {
+      this.totalCount = data.rspBody.size;
+      this.KRM046Source.data = data.rspBody.items;
+    });
   }
 
 }
