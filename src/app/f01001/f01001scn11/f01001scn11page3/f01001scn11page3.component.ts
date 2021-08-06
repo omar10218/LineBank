ea671b2e86f2baf3592cb87819f82005d0b997f7
omar10218 <omar10218@gmail.com>
2021-08-06 11:45:49 +0800

    微調

--- src/app/f01001/f01001scn11/f01001scn11page3/f01001scn11page3.component.ts
+++ src/app/f01001/f01001scn11/f01001scn11page3/f01001scn11page3.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MappingCode } from 'src/app/mappingcode.model';
 import { F01001scn11Service } from '../f01001scn11.service';
+interface Code {
+  compareColumn: string;
+  result: string;
+  count: string;
+}
 
 @Component({
   selector: 'app-f01001scn11page3',
@@ -11,37 +17,9 @@ import { F01001scn11Service } from '../f01001scn11.service';
 export class F01001scn11page3Component implements OnInit {
 
   private applno: string;
-
-  historySameIDForm: FormGroup = this.fb.group({
-    gps1 : ['', []],
-    gps2 : ['', []],
-    ipAddr1 : ['', []],
-    ipAddr2 : ['', []],
-    phoneModel1 : ['', []],
-    phoneModel2 : ['', []],
-    deviceID1 : ['', []],
-    deviceID2 : ['', []],
-    email : ['', []],
-    mTel : ['', []],
-    pTel : ['', []],
-    cTel : ['', []],
-    pAddr : ['', []],
-    pAddrFuzzy : ['', []],
-    cAddr : ['', []],
-    cAddrFuzzy : ['', []],
-    mAddr : ['', []],
-    mAddrFuzzy : ['', []],
-    cpName : ['', []],
-    cpTel : ['', []],
-    cpAddr : ['', []],
-    cpAddrFuzzy : ['', []],
-    cpType : ['', []],
-    cpTypeLevel1 : ['', []],
-    cpTypeLevel1and2 : ['', []],
-    cpTitle : ['', []],
-    education : ['', []]
-  });
-
+  mappingOption: MappingCode[];
+  compare: Code[] = [];
+ 
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private f01001scn11Service: F01001scn11Service) { }
 
   ngOnInit(): void {
@@ -57,62 +35,18 @@ export class F01001scn11page3Component implements OnInit {
     formdata.append('applno', this.applno);
     formdata.append('code', 'EL_HISTORY_COMPARE_SAMEID');
     this.f01001scn11Service.getCompare(formdata).subscribe(data => {
-      console.log(data.rspBody.compare);
-      let td_gps1 = document.querySelectorAll(".td_gps1") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ gps1: data.rspBody.items[0].GPS_1 });  if ( data.rspBody.items[0].GPS_1 == 1 ) { td_gps1.item(0).style.color = "red"; } 
-      let td_gps2 = document.querySelectorAll(".td_gps2") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ gps2: data.rspBody.items[0].GPS_2 });
-      let td_ipAddr1 = document.querySelectorAll(".td_ipAddr1") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ ipAddr1: data.rspBody.items[0].IP_ADDR_1 });
-      let td_ipAddr2 = document.querySelectorAll(".td_ipAddr2") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ ipAddr2: data.rspBody.items[0].IP_ADDR_2 });
-      let td_phoneModel1 = document.querySelectorAll(".td_phoneModel1") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ phoneModel1: data.rspBody.items[0].PHONE_MODEL_1 });
-      let td_phoneModel2 = document.querySelectorAll(".td_phoneModel2") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ phoneModel2: data.rspBody.items[0].PHONE_MODEL_2 });
-      let td_deviceID1 = document.querySelectorAll(".td_deviceID1") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ deviceID1: data.rspBody.items[0].DEVICE_ID_1 });
-      let td_deviceID2 = document.querySelectorAll(".td_deviceID2") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ deviceID2: data.rspBody.items[0].DEVICE_ID_2 });
-      let td_email = document.querySelectorAll(".td_email") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ email: data.rspBody.items[0].EMAIL });
-      let td_mTel = document.querySelectorAll(".td_mTel") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ mTel: data.rspBody.items[0].M_TEL });
-      let td_pTel = document.querySelectorAll(".td_pTel") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ pTel: data.rspBody.items[0].P_TEL });
-      let td_cTel = document.querySelectorAll(".td_cTel") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cTel: data.rspBody.items[0].C_TEL });
-      let td_pAddr = document.querySelectorAll(".td_pAddr") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ pAddr: data.rspBody.items[0].P_ADDR });
-      let td_pAddrFuzzy = document.querySelectorAll(".td_pAddrFuzzy") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ pAddrFuzzy: data.rspBody.items[0].P_ADDR_FUZZY });
-      let td_cAddr = document.querySelectorAll(".td_cAddr") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cAddr: data.rspBody.items[0].C_ADDR });
-      let td_cAddrFuzzy = document.querySelectorAll(".td_cAddrFuzzy") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cAddrFuzzy: data.rspBody.items[0].C_ADDR_FUZZY });
-      let td_mAddr = document.querySelectorAll(".td_mAddr") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ mAddr: data.rspBody.items[0].M_ADDR });
-      let td_mAddrFuzzy = document.querySelectorAll(".td_mAddrFuzzy") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ mAddrFuzzy: data.rspBody.items[0].M_ADDR_FUZZY });
-      let td_cpName = document.querySelectorAll(".td_cpName") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpName: data.rspBody.items[0].CP_NAME });
-      let td_cpTel = document.querySelectorAll(".td_cpTel") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpTel: data.rspBody.items[0].CP_TEL });
-      let td_cpAddr = document.querySelectorAll(".td_cpAddr") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpAddr: data.rspBody.items[0].CP_ADDR });
-      let td_cpAddrFuzzy = document.querySelectorAll(".td_cpAddrFuzzy") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpAddrFuzzy: data.rspBody.items[0].CP_ADDR_FUZZY });
-      let td_cpType = document.querySelectorAll(".td_cpType") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpType: data.rspBody.items[0].CP_TYPE });
-      let td_cpTypeLevel1 = document.querySelectorAll(".td_cpTypeLevel1") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpTypeLevel1: data.rspBody.items[0].CP_TYPE_LEVEL_1 });
-      let td_cpTypeLevel1and2 = document.querySelectorAll(".td_cpTypeLevel1and2") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpTypeLevel1and2: data.rspBody.items[0].CP_TYPE_LEVEL_1_2 });
-      let td_cpTitle = document.querySelectorAll(".td_cpTitle") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ cpTitle: data.rspBody.items[0].CP_TITLE });
-      let td_education = document.querySelectorAll(".td_education") as NodeListOf<HTMLElement>;
-      this.historySameIDForm.patchValue({ education: data.rspBody.items[0].EDUCATION });
+      this.mappingOption = data.rspBody.table;
+      this.compare = data.rspBody.compare;
     });
   }
 
+  getOptionDesc(codeVal: string): string {
+    for (const data of this.mappingOption) {
+      if (data.codeNo == codeVal) {
+        return data.codeDesc;
+        break;
+      }
+    }
+    return codeVal;
+  }
 }
--- src/app/f01001/f01001scn11/f01001scn11page5/f01001scn11page5.component.ts
+++ src/app/f01001/f01001scn11/f01001scn11page5/f01001scn11page5.component.ts
@@ -1,7 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { MappingCode } from 'src/app/mappingcode.model';
 import { F01001scn11Service } from '../f01001scn11.service';
+interface Code {
+  compareColumn: string;
+  result: string;
+  count: string;
+}
 
 @Component({
   selector: 'app-f01001scn11page5',
@@ -11,42 +17,8 @@ import { F01001scn11Service } from '../f01001scn11.service';
 export class F01001scn11page5Component implements OnInit {
 
   private applno: string;
-
-  bam061Form: FormGroup = this.fb.group({
-    mobileVsMTel6: ['', []],
-    mobileVsMTel12: ['', []],
-    pTelVsHTel6: ['', []],
-    pTelVsHTel12: ['', []],
-    pTelVsOTel6: ['', []],
-    pTelVsOTel12: ['', []],
-    cTelVsHTel6: ['', []],
-    cTelVsHTel12: ['', []],
-    cTelVsOTel6: ['', []],
-    cTelVsOTel12: ['', []],
-    pAddrVsRAddr6: ['', []],
-    pAddrVsRAddr12: ['', []],
-    pAddrVsMAddr6: ['', []],
-    pAddrVsMAddr12: ['', []],
-    pAddrVsRAddrFuzzy6: ['', []],
-    pAddrVsRAddrFuzzy12: ['', []],
-    pAddrVsMAddrFuzzy6: ['', []],
-    cAddrVsRAddr6: ['', []],
-    cAddrVsMAddr6: ['', []],
-    cAddrVsRAddrFuzzy6: ['', []],
-    cAddrVsMAddrFuzzy6: ['', []],
-    cardAddrVsRAddr6: ['', []],
-    cardAddrVsMAddr6: ['', []],
-    cardAddrVsRAddrFuzzy6: ['', []],
-    cardAddrVsMAddrFuzzy6: ['', []],
-    cpName: ['', []],
-    cpTelVsHTel: ['', []],
-    cpTelVsOTel: ['', []],
-    cpAddrVsRAddr6: ['', []],
-    cpAddrVsMAddr6: ['', []],
-    cpAddrVsRAddrFuzzy6: ['', []],
-    cpAddrVsMAddrFuzzy6: ['', []],
-    education: ['', []]
-  });
+  mappingOption: MappingCode[];
+  compare: Code[] = [];
 
   constructor(private route: ActivatedRoute, private fb: FormBuilder, private f01001scn11Service: F01001scn11Service) { }
 
@@ -63,74 +35,19 @@ export class F01001scn11page5Component implements OnInit {
     formdata.append('applno', this.applno);
     formdata.append('code', 'EL_BAM061_COMPARE');
     this.f01001scn11Service.getCompare(formdata).subscribe(data => {
-      console.log(data.rspBody.compare)
-      let td_mobileVsMTel6 = document.querySelectorAll(".td_mobileVsMTel6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ mobileVsMTel6: data.rspBody.items[0].MOBILE_VS_M_TEL_6_MONTH });  if ( data.rspBody.compare[0].compareColumn == 'MOBILE_VS_M_TEL_6_MONTH' && data.rspBody.compare[0].setValue < data.rspBody.items[0].MOBILE_VS_M_TEL_6_MONTH  ) { td_mobileVsMTel6.item(0).style.color = "red"; } 
-      let td_mobileVsMTel12 = document.querySelectorAll(".td_mobileVsMTel12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ mobileVsMTel12: data.rspBody.items[0].MOBILE_VS_M_TEL_12_MONTH });
-      let td_pTelVsHTel6 = document.querySelectorAll(".td_pTelVsHTel6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pTelVsHTel6: data.rspBody.items[0].P_TEL_VS_H_TEL_6_MONTH });
-      let td_pTelVsHTel12 = document.querySelectorAll(".td_pTelVsHTel12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pTelVsHTel12: data.rspBody.items[0].P_TEL_VS_H_TEL_12_MONTH });
-      let td_pTelVsOTel6 = document.querySelectorAll(".td_pTelVsOTel6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pTelVsOTel6: data.rspBody.items[0].P_TEL_VS_O_TEL_6_MONTH });
-      let td_pTelVsOTel12 = document.querySelectorAll(".td_pTelVsOTel12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pTelVsOTel12: data.rspBody.items[0].P_TEL_VS_O_TEL_12_MONTH });
-      let td_cTelVsHTel6 = document.querySelectorAll(".td_cTelVsHTel6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cTelVsHTel6: data.rspBody.items[0].C_TEL_VS_H_TEL_6_MONTH });
-      let td_cTelVsHTel12 = document.querySelectorAll(".td_cTelVsHTel12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cTelVsHTel12: data.rspBody.items[0].C_TEL_VS_H_TEL_12_MONTH });
-      let td_cTelVsOTel6 = document.querySelectorAll(".td_cTelVsOTel6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cTelVsOTel6: data.rspBody.items[0].C_TEL_VS_O_TEL_6_MONTH });
-      let td_cTelVsOTel12 = document.querySelectorAll(".td_cTelVsOTel12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cTelVsOTel12: data.rspBody.items[0].C_TEL_VS_O_TEL_12_MONTH });
-      let td_pAddrVsRAddr6 = document.querySelectorAll(".td_pAddrVsRAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsRAddr6: data.rspBody.items[0].P_ADDR_VS_R_ADDR_6_MONTH });
-      let td_pAddrVsRAddr12 = document.querySelectorAll(".td_pAddrVsRAddr12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsRAddr12: data.rspBody.items[0].P_ADDR_VS_R_ADDR_12_MONTH });
-      let td_pAddrVsMAddr6 = document.querySelectorAll(".td_pAddrVsMAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsMAddr6: data.rspBody.items[0].P_ADDR_VS_M_ADDR_6_MONTH });
-      let td_pAddrVsMAddr12 = document.querySelectorAll(".td_pAddrVsMAddr12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsMAddr12: data.rspBody.items[0].P_ADDR_VS_M_ADDR_12_MONTH });
-      let td_pAddrVsRAddrFuzzy6 = document.querySelectorAll(".td_pAddrVsRAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsRAddrFuzzy6: data.rspBody.items[0].P_ADDR_VS_R_ADDR_FUZZY_6_MONTH });
-      let td_pAddrVsRAddrFuzzy12 = document.querySelectorAll(".td_pAddrVsRAddrFuzzy12") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsRAddrFuzzy12: data.rspBody.items[0].P_ADDR_VS_R_ADDR_FUZZY_12_MONTH });
-      let td_pAddrVsMAddrFuzzy6 = document.querySelectorAll(".td_pAddrVsMAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ pAddrVsMAddrFuzzy6: data.rspBody.items[0].P_ADDR_VS_M_ADDR_FUZZY_6_MONTH });
-      let td_cAddrVsRAddr6 = document.querySelectorAll(".td_cAddrVsRAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cAddrVsRAddr6: data.rspBody.items[0].C_ADDR_VS_R_ADDR_6_MONTH });
-      let td_cAddrVsMAddr6 = document.querySelectorAll(".td_cAddrVsMAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cAddrVsMAddr6: data.rspBody.items[0].C_ADDR_VS_M_ADDR_6_MONTH });
-      let td_cAddrVsRAddrFuzzy6 = document.querySelectorAll(".td_cAddrVsRAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cAddrVsRAddrFuzzy6: data.rspBody.items[0].C_ADDR_VS_R_ADDR_FUZZY_6_MONTH });
-      let td_cAddrVsMAddrFuzzy6 = document.querySelectorAll(".td_cAddrVsMAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cAddrVsMAddrFuzzy6: data.rspBody.items[0].C_ADDR_VS_M_ADDR_FUZZY_6_MONTH });
-      let td_cardAddrVsRAddr6 = document.querySelectorAll(".td_cardAddrVsRAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cardAddrVsRAddr6: data.rspBody.items[0].CARD_ADDR_VS_R_ADDR_6_MONTH });
-      let td_cardAddrVsMAddr6 = document.querySelectorAll(".td_cardAddrVsMAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cardAddrVsMAddr6: data.rspBody.items[0].CARD_ADDR_VS_M_ADDR_6_MONTH });
-      let td_cardAddrVsRAddrFuzzy6 = document.querySelectorAll(".td_cardAddrVsRAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cardAddrVsRAddrFuzzy6: data.rspBody.items[0].CARD_ADDR_VS_R_ADDR_FUZZY_6_MONTH });
-      let td_cardAddrVsMAddrFuzzy6 = document.querySelectorAll(".td_cardAddrVsMAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cardAddrVsMAddrFuzzy6: data.rspBody.items[0].CARD_ADDR_VS_M_ADDR_FUZZY_6_MONTH });
-      let td_cpName = document.querySelectorAll(".td_cpName") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpName: data.rspBody.items[0].CP_NAME });
-      let td_cpTelVsHTel = document.querySelectorAll(".td_cpTelVsHTel") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpTelVsHTel: data.rspBody.items[0].CP_TEL_VS_H_TEL });
-      let td_cpTelVsOTel = document.querySelectorAll(".td_cpTelVsOTel") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpTelVsOTel: data.rspBody.items[0].CP_TEL_VS_O_TEL });
-      let td_cpAddrVsRAddr6 = document.querySelectorAll(".td_cpAddrVsRAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpAddrVsRAddr6: data.rspBody.items[0].CP_ADDR_VS_R_ADDR_6_MONTH });
-      let td_cpAddrVsMAddr6 = document.querySelectorAll(".td_cpAddrVsMAddr6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpAddrVsMAddr6: data.rspBody.items[0].CP_ADDR_VS_M_ADDR_6_MONTH });
-      let td_cpAddrVsRAddrFuzzy6 = document.querySelectorAll(".td_cpAddrVsRAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpAddrVsRAddrFuzzy6: data.rspBody.items[0].CP_ADDR_VS_R_ADDR_FUZZY_6_MONTH });
-      let td_cpAddrVsMAddrFuzzy6 = document.querySelectorAll(".td_cpAddrVsMAddrFuzzy6") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ cpAddrVsMAddrFuzzy6: data.rspBody.items[0].CP_ADDR_VS_M_ADDR_FUZZY_6_MONTH });
-      let td_education = document.querySelectorAll(".td_education") as NodeListOf<HTMLElement>;
-      this.bam061Form.patchValue({ education: data.rspBody.items[0].EDUCATION });
+      this.mappingOption = data.rspBody.table;
+      this.compare = data.rspBody.compare;
     });
+  }
+
+  getOptionDesc(codeVal: string): string {
+    for (const data of this.mappingOption) {
+      if (data.codeNo == codeVal) {
+        return data.codeDesc;
+        break;
+      }
+    }
+    return codeVal;
   }
 
 }
