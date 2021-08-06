fec3b10c77570b50bec4359f1b527f52c442a959
a0188218 <a0188218@hotmail.com>
2021-08-06 16:54:46 +0800

    20210806-1

--- src/app/f03012/f03012edit/f03012edit.component.ts
+++ src/app/f03012/f03012edit/f03012edit.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { F03012Service } from '../f03012.service';
+import { F03012confirmComponent } from '../f03012confirm/f03012confirm.component';
 
+interface sysCode {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-f03012edit',
   templateUrl: './f03012edit.component.html',
   styleUrls: ['./f03012edit.component.css']
 })
 export class F03012editComponent implements OnInit {
+  selectedValue1: string;
+  selectedValue2: string;
+  setValue: string;
+  compareTableCode: sysCode[] = [];
+  compareColumnCode: sysCode[] = [];
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<F03012editComponent>, private f03012Service: F03012Service, public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-  ngOnInit(): void {
+
+
+  formControl = new FormControl('', [
+    Validators.required
+    // Validators.email,
+  ]);
+
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? 'Required field' : '';
   }
 
+  ngOnInit(): void {
+    console.log(this.data.compareColumn)
+    console.log(this.data.setValue)
+    this.f03012Service.getSysTypeCode('COMPARE_TABLE', 'f03/f03012')
+      .subscribe(data => {
+        for (const jsonObj of data.rspBody) {
+          const codeNo = jsonObj['codeNo'];
+          const desc = jsonObj['codeDesc'];
+          this.compareTableCode.push({ value: codeNo, viewValue: desc })
+        }
+      });
+    this.f03012Service.getSysTypeCode(this.data.compareTable, 'f03/f03012')
+      .subscribe(data => {
+        for (const jsonObj of data.rspBody) {
+          const codeNo = jsonObj['codeNo'];
+          const desc = jsonObj['codeDesc'];
+          this.compareColumnCode.push({ value: codeNo, viewValue: desc })
+        }
+      });
+  }
+
+  public async save(): Promise<void> {
+    let msgStr: string = "";
+    let baseUrl = 'f03/f03012action2';
+    msgStr = await this.f03012Service.update(baseUrl, this.data);
+    const childernDialogRef = this.dialog.open(F03012confirmComponent, {
+      data: { msgStr: msgStr }
+    });
+    if (msgStr === '儲存成功！') { this.dialogRef.close({ event: 'success' }); }
+  }
+
+
+
+
+
+
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
