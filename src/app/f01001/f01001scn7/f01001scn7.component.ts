c49efb80eaaccc4a9ba967235294d06eee1ba075
jir3926 <jir3926@gmail.com>
2021-08-16 17:49:45 +0800

    新增 AML-FDS-CSS

--- src/app/f01001/f01001scn7/f01001scn7.component.ts
+++ src/app/f01001/f01001scn7/f01001scn7.component.ts
@@ -1,14 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { F01001scn7Service } from './f01001scn7.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-f01001scn7',
   templateUrl: './f01001scn7.component.html',
-  styleUrls: ['./f01001scn7.component.css']
+  styleUrls: ['./f01001scn7.component.css', '../../../assets/css/f01.css']
 })
 export class F01001scn7Component implements OnInit {
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+   CALLOUTSource = new MatTableDataSource<any>();
+  AMLSource:any;
+  FDSSource:any;
+  CSSSource:any;
+
+
+
+  constructor(private route: ActivatedRoute, private router: Router, private f01001scn7Service: F01001scn7Service) { }
   private applno: string;
   private search: string;
   ngOnInit(): void {
@@ -16,6 +25,7 @@ export class F01001scn7Component implements OnInit {
       this.applno = params['applno'];
       this.search = params['search'];
     });
+    this.getCALLOUTFunction();
   }
 
   getApplno(): String {
@@ -25,4 +35,17 @@ export class F01001scn7Component implements OnInit {
   getSearch() :string {
     return this.search;
   }
+
+
+  private async getCALLOUTFunction() {
+    const baseUrl = 'f01/f01001scn7';
+    this.f01001scn7Service.getAML_FDS_CSS(baseUrl,this.applno).subscribe(data => {
+       console.log(data);
+       this.AMLSource=data.rspBody.amlList;
+       this.FDSSource= data.rspBody.fdsList;
+       this.CSSSource= data.rspBody.cssList;
+
+    });
+  }
+
 }
