58f14e07d314207d7318e8daef0cc708d09d94a5
a0188218 <a0188218@hotmail.com>
2021-08-24 11:52:58 +0800

    案件清單

--- src/app/f01003/f01003.service.ts
+++ src/app/f01003/f01003.service.ts
@@ -6,12 +6,12 @@ import { BaseService } from '../base.service';
 @Injectable({
   providedIn: 'root'
 })
-export class F01002Service extends BaseService {
+export class F01003Service extends BaseService {
   constructor(protected httpClient: HttpClient) { super(httpClient); }
   getCaseList(pageIndex: number, pageSize: number, empno: string, swcID: string, swcApplno: string): Observable<any> {
-    const baseUrl = 'f01/f01002';
-    let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}&swcL3EmpNo=${empno}&swcID=${swcID}&swcApplno=${swcApplno}`;
+    const baseUrl = 'f01/f01003';
+    let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}&swcL2EmpNo=${empno}&swcID=${swcID}&swcApplno=${swcApplno}`;
     return this.postHttpClient(targetUrl);
   }
-  
+
 }
