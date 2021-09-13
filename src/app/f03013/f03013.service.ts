a0caa6e0fc7da943b3149cf2ef50562ecfb0b1f3
a0188218 <a0188218@hotmail.com>
2021-09-13 13:36:54 +0800

    20210913 工作日維護

---
+++ src/app/f03013/f03013.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BaseService } from '../base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class F03013Service extends BaseService{
+
+  constructor(protected httpClient: HttpClient) { super(httpClient); }
+
+  createCalendar(year: string){
+    const baseUrl = 'f03/f03013';
+    let targetUrl = `${baseUrl}?year=${year}`;
+    return this.postHttpClient(targetUrl);
+  }
+  queryIsWorkDay(year: string, month: string){
+    const baseUrl = 'f03/f03013action1';
+    let targetUrl = `${baseUrl}?year=${year}?month=${month}`;
+    return this.postHttpClient(targetUrl);
+  }
+  updateWorkingDate(wDate: string, isWork: string){
+    const baseUrl = 'f03/f03013action2';
+    let targetUrl = `${baseUrl}?wDate=${wDate}?isWork=${isWork}`;
+    return this.postHttpClient(targetUrl);
+  }
+}
