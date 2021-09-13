d672aefbd3acd07bc48a7e5fb4bd4c81483894c1
omar10218 <omar10218@gmail.com>
2021-09-13 16:15:27 +0800

    test

---
+++ src/app/f03015/f03015.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f03015',
+  templateUrl: './f03015.component.html',
+  styleUrls: ['./f03015.component.css']
+})
+export class F03015Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/f03015/f03015confirm/f03015confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f03015confirm',
+  templateUrl: './f03015confirm.component.html',
+  styleUrls: ['./f03015confirm.component.css']
+})
+export class F03015confirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
