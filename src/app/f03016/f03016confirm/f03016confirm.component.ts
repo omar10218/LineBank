f0eb42a0ca5b8773599064b6051fb7a12de6487d
omar10218 <omar10218@gmail.com>
2021-09-13 17:09:55 +0800

    test

---
+++ src/app/f03016/f03016confirm/f03016confirm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f03016confirm',
+  templateUrl: './f03016confirm.component.html',
+  styleUrls: ['./f03016confirm.component.css']
+})
+export class F03016confirmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
