ebf29d9f68c6aab164c9f3dff8be22bb06b6cf37
Mount.Lin <mount.lin@rstn.com.tw>
2021-09-23 09:52:38 +0800

    上傳excel

--- src/app/f03015/f03015.service.ts
+++ src/app/f03015/f03015.service.ts
@@ -6,46 +6,43 @@ import { BaseService } from '../base.service';
 @Injectable({
   providedIn: 'root'
 })
-export class F03015Service extends BaseService{
+export class F03015Service extends BaseService {
 
   constructor(protected httpClient: HttpClient) { super(httpClient); }
-
-  // getProxtIncome(baseUrl: string, pageIndex: number, pageSize: number, indcuCode: string, inducLevel1: string, inducLevel2: string): Observable<any> {
-  //   let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}&reasonKind=${indcuCode}&upReasonCode=${inducLevel1}&level=${inducLevel2}`;
-  //   console.log(targetUrl)
-  //   return this.postHttpClient(targetUrl);
-  // }
 
   selectCustomer(baseUrl: string, formData: FormData): Observable<any> {
     return this.postFormData(baseUrl, formData);
   }
 
-  getReturn(baseUrl: string, jsonObject:JSON): Observable<any> {
-    // const baseUrl = 'f03/f03015';
+  getReturn(baseUrl: string, jsonObject: JSON): Observable<any> {
     let targetUrl = `${baseUrl}`;
 
     return this.postJsonObject(targetUrl, jsonObject);
   }
 
-  downloadExcel(baseUrl: string, jsonObject:JSON): Observable<any> {
-    // const baseUrl = 'f03/f03015';
+  downloadExcel(baseUrl: string, jsonObject: JSON): Observable<any> {
     let targetUrl = `${baseUrl}`;
 
     return this.postGetFile(targetUrl, jsonObject);
   }
 
+  uploadExcel(baseUrl: string, formdata: any) {
 
-  insert(baseUrl: string, jsonObject:JSON): Observable<any> {
-    // const baseUrl = 'f03/f03015';
+  }
+  insert(baseUrl: string, jsonObject: JSON): Observable<any> {
     let targetUrl = `${baseUrl}`;
 
     return this.postJsonObject(targetUrl, jsonObject);
   }
 
-  download(baseUrl: string, file: string | undefined): Observable<Blob> {
-    // return this.http.get(`${environment.baseUrl}/files/${file}`, {
-    return this.httpClient.get(baseUrl, {
-      responseType: 'blob'
-    });
+  postFile(fileToUpload: File): void {
+    const endpoint = 'your-destination-url';
+    const formData: FormData = new FormData();
+    formData.append('fileKey', fileToUpload, fileToUpload.name);
+    // return this.httpClient
+    //   .post(endpoint, formData, { headers: '' })
+    //   .map(() => { return true; })
+    //   .catch((e) => this.handleError(e));
   }
+
 }
--- src/app/f03015/f03015upload/f03015upload.component.ts
+++ src/app/f03015/f03015upload/f03015upload.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmComponent } from 'src/app/common-lib/confirm/confirm.component';
+import { F03015Service } from '../f03015.service';
 
 @Component({
   selector: 'app-f03015upload',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class F03015uploadComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(public dialogRef: MatDialogRef<F03015uploadComponent>,private f03015Service: F03015Service,public dialog: MatDialog,) { }
+  @ViewChild('inputFile') inputFile: ElementRef;
+  isExcelFile: boolean;
+  spinnerEnabled = false;
+  fileToUpload: File | null = null;
   ngOnInit(): void {
   }
 
+  public async confirmAdd(): Promise<void> {
+    const formdata: FormData = new FormData();
+    formdata.append('myFile', this.fileToUpload);
+    console.log(formdata)
+    let msgStr: string = "";
+    let baseUrl = 'f03/f03015action4';
+    await this.f03015Service.uploadExcel(baseUrl, formdata);
+    const childernDialogRef = this.dialog.open(ConfirmComponent, {
+      data: { msgStr: "傳送" }
+    });
+    if (msgStr === '上傳成功!!') { this.dialogRef.close({ event: 'success' }); }
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+  uploadFileToActivity() {
+    // this.f03015service.postFile(this.fileToUpload).subscribe(data => {
+    //   // do something, if upload success
+    //   }, error => {
+    //     console.log(error);
+    //   });
+  }
+  submit() {
+  }
+  onChange(evt) {
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    this.isExcelFile = !!target.files[0].name.match(/(.xls|.xlsx)/);
+    if (target.files.length > 1) {
+      this.inputFile.nativeElement.value = '';
+    }
+    if (this.isExcelFile) {
+      console.log("我是EXCEL")
+      const reader: FileReader = new FileReader();
+      reader.onload = (e: any) => {
+      };
+
+      reader.readAsBinaryString(target.files[0]);
+    }
+  }
 }
