0461e05d7a97f65265ca5775cebf48a881ce2df2
Mount.Lin <mount.lin@rstn.com.tw>
2021-09-24 20:30:13 +0800

    提報黑名單

---
+++ src/app/f01002/f01002blocklist/f01002blocklist.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BaseService } from 'src/app/base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class F01002BlockListService extends BaseService{
+
+  constructor(protected httpClient: HttpClient) { super(httpClient); }
+
+  gettable(baseUrl: string, applno: string) {
+    let targetUrl = `${baseUrl}?applno=${applno}`;
+    return this.postHttpClient(targetUrl);
+  }
+  oneseve(baseUrl: string,json:JSON) {
+  let targetUrl = baseUrl;
+  return this.postJsonObject(targetUrl,json);
+  }
+  
+}
