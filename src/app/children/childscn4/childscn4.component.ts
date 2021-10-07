fa776aa173461216eef8c06e27204cc937d9f88f
omar10218 <omar10218@gmail.com>
2021-10-07 17:47:12 +0800

    案件歷程

--- src/app/children/childscn4/childscn4.component.ts
+++ src/app/children/childscn4/childscn4.component.ts
@@ -3,13 +3,15 @@ import { MatDialog } from '@angular/material/dialog';
 import { PageEvent, MatPaginator } from '@angular/material/paginator';
 import { Sort, MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Data } from '@angular/router';
+import { NzTableQueryParams } from 'ng-zorro-antd/table';
 import { Childscn4Service } from './childscn4.service';
 
 
 @Component({
   selector: 'app-childscn4',
   templateUrl: './childscn4.component.html',
-  styleUrls: ['./childscn4.component.css','../../../assets/css/f01.css']
+  styleUrls: ['./childscn4.component.css','../../../assets/css/child.css']
 })
 export class Childscn4Component implements OnInit {
 
@@ -18,56 +20,37 @@ export class Childscn4Component implements OnInit {
     public dialog: MatDialog
   ) { }
 
+  caseStepSource: Data[] = [];
+  total = 1;
+  loading = true;
+  pageSize = 10;
+  pageIndex = 1;
+
   private applno: string;
-  private search: string;
-  currentPage: PageEvent;
-  currentSort: Sort;
-  caseStepSource = new MatTableDataSource<any>();
 
   ngOnInit(): void {
     this.applno = sessionStorage.getItem('applno');
-    this.search = sessionStorage.getItem('search');
-
-    this.currentPage = {
-      pageIndex: 0,
-      pageSize: 10,
-      length: null
-    };
-
-    this.currentSort = {
-      active: '',
-      direction: ''
-    };
   }
-
-  getApplno(): String {
-    return this.applno;
-  }
-
-  getSearch() :string {
-    return this.search;
-  }
-
-  totalCount: any;
-  @ViewChild('paginator', { static: true }) paginator: MatPaginator;
-  @ViewChild('sortTable', { static: true }) sortTable: MatSort;
 
   ngAfterViewInit() {
-    this.getCaseStep();
-    this.paginator.page.subscribe((page: PageEvent) => {
-      this.currentPage = page;
-      this.getCaseStep();
+    this.getCaseStep( this.pageIndex, this.pageSize );
+  }
+
+  getCaseStep( pageIndex: number, pageSize: number ){
+    const baseUrl = 'f01/childscn4';
+    let jsonObject: any = {};
+    jsonObject['applno'] = this.applno;
+    jsonObject['page'] = this.pageIndex;
+    jsonObject['per_page'] = this.pageSize;
+    this.childscn4Service.getCaseStep( baseUrl, jsonObject ).subscribe(data => {
+      this.loading = false;
+      this.total = data.rspBody.size;
+      this.caseStepSource = data.rspBody.items;
     });
   }
 
-  getCaseStep(){
-    const formdata: FormData = new FormData();
-    formdata.append('page', `${this.currentPage.pageIndex + 1}`);
-    formdata.append('per_page', `${this.currentPage.pageSize}`);
-    formdata.append('applno', this.applno);
-    this.childscn4Service.getCaseStep(formdata).subscribe(data => {
-      this.totalCount = data.rspBody.size;
-      this.caseStepSource.data = data.rspBody.items;
-    });
+  onQueryParamsChange(params: NzTableQueryParams): void {
+    const { pageSize, pageIndex } = params;
+    this.getCaseStep(pageIndex, pageSize);
   }
 }
