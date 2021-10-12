76a938cd585c046d9bab48126b7c07c3edba37ab
omar10218 <omar10218@gmail.com>
2021-10-12 10:03:20 +0800

    test

---
+++ src/app/f01007/f01007.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f01007',
+  templateUrl: './f01007.component.html',
+  styleUrls: ['./f01007.component.css']
+})
+export class F01007Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
