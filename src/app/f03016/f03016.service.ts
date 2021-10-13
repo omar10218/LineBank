d7e2f9dd4cab9ba2f88785b7346146c523077cc6
jeremyChangZhiChao <jeremy.chang@rstn.com.tw>
2021-10-13 18:28:59 +0800

    調整傳到後段參數名稱

--- src/app/f03016/f03016.service.ts
+++ src/app/f03016/f03016.service.ts
@@ -9,21 +9,11 @@ export class F03016Service extends BaseService {
 
   constructor(protected httpClient: HttpClient) { super(httpClient); }
 
-  getImpertmentParameter(baseUrl: string, json: JSON): Observable<any> {
-    // let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}`;
-    // return this.postHttpClient(targetUrl);
-    return this.postJsonObject(baseUrl, json);
+  getImpertmentParameter(baseUrl: string, jsonObject: JSON): Observable<any> {
+    return this.postJsonObject(baseUrl, jsonObject);
   }
 
   update(baseUrl: string, jsonObject: any): any {
-
-    jsonObject['DssJcicSet'] = jsonObject.DssJcicSet;
-    jsonObject['DssMailDay'] = jsonObject.DssMailDay;
-    jsonObject['BasicLimit'] = jsonObject.BasicLimit;
-    jsonObject['CssPassStart'] = jsonObject.CssPassStart;
-    jsonObject['CssPassEnd'] = jsonObject.CssPassEnd;
-    jsonObject['IsJcic'] = jsonObject.IsJcic;
-    jsonObject['TransEmpNo'] = jsonObject.TransEmpNo;
 
     return this.saveOrEditMsgString(baseUrl, jsonObject);
 
