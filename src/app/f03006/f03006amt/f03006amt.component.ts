07d21a4d7cdb16583d2cb0bf7d51b0c35a0b4b0a
omar10218 <omar10218@gmail.com>
2021-10-14 15:36:32 +0800

    test

---
+++ src/app/f03006/f03006amt/f03006amt.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f03006amt',
+  templateUrl: './f03006amt.component.html',
+  styleUrls: ['./f03006amt.component.css']
+})
+export class F03006amtComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
