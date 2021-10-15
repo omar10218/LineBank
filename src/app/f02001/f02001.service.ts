e1e8706381ffb7d3d7e32a1614c158cefe063e1a
jeremyChangZhiChao <jeremy.chang@rstn.com.tw>
2021-10-15 13:47:07 +0800

    mappingcode下拉選單改為前端方法取得

--- src/app/f02001/f02001.service.ts
+++ src/app/f02001/f02001.service.ts
@@ -13,9 +13,15 @@ export class F02001Service extends BaseService {
     let targetUrl = `${baseUrl}?page=${pageIndex + 1}&per_page=${pageSize}`;
     return this.postFormData(targetUrl, formData);
   }
-  inquiry(baseUrl: string,json:JSON)
-  {
+
+  getStatusDesc(): Observable<any>{
+    let targetUrl = 'f02/f02001';
+    let jsonObject: any = {};
+    return this.postJsonObject(targetUrl, jsonObject);
+  }
+
+  inquiry(baseUrl: string, jsonObject: JSON): Observable<any> {
     let targetUrl = baseUrl;
-    return this.postJsonObject(targetUrl,json);
+    return this.postJsonObject(targetUrl, jsonObject);
   }
 }
