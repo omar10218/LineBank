458b43f4dd4a0901401d16211f56a4fa2486351f
a0188218 <a0188218@hotmail.com>
2021-10-20 15:52:28 +0800

    20211020更新

--- src/app/children/childscn5/childscn5.service.ts
+++ src/app/children/childscn5/childscn5.service.ts
@@ -10,14 +10,18 @@ export class Childscn5Service extends BaseService {
 
   constructor(protected httpClient: HttpClient) { super(httpClient); }
 
-  getCustomerInfoSearch(formData: FormData): Observable<any>  {
+  getCustomerInfoSearch(jsonObject: JSON): Observable<any>  {
+
     const baseUrl = 'f01/childscn5';
-    return this.postFormData(baseUrl, formData);
+
+    return this.postJsonObject(baseUrl, jsonObject);
   }
 
-  update(baseUrl: string, data: any): any {
-    const formdata: FormData = new FormData();
-    return this.saveOrEditMsgString(baseUrl, formdata);
+  update(jsonObject: JSON): any {
+
+    const baseUrl = 'f01/childscn5action1';
+
+    return this.postJsonObject(baseUrl, jsonObject);
   }
 
 }
