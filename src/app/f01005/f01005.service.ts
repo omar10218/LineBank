a9ae65b77d76233f4ad8bced8554f6d7ab8b6db3
mopackj43266 <42729635+mopackj43266@users.noreply.github.com>
2021-10-21 09:14:49 +0800

    f01005 update

---
+++ src/app/f01005/f01005.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BaseService } from '../base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class F01005Service extends BaseService {
+  constructor(protected httpClient: HttpClient) { super(httpClient); }
+  // pageIndex: number, pageSize: number, empno: string, swcID: string, swcApplno: string
+  getCaseList(jsonObject: JSON): Observable<any> {
+    const baseUrl = 'f01/f01005';
+
+    return this.postJsonObject(baseUrl, jsonObject);
+  }
+  getEmpNo(jsonObject: JSON): Observable<any> {
+    const baseUrl = 'f01/f01005fn2';
+
+    return this.postJsonObject(baseUrl, jsonObject);
+  }
+  getLockCase(jsonObject: JSON) {
+    const baseUrl = 'f01/f01005fn1';
+
+    return this.postJsonObject(baseUrl, jsonObject);
+  }
+
+  saveCaseMemo(jsonObject: JSON) {
+    const baseUrl = 'f01/f01005fn3';
+    return this.postJsonObject(baseUrl, jsonObject);
+  }
+
+  getCalloutList(jsonObject: JSON): Observable<any> {
+    const baseUrl = 'f01/f01005fn4';
+
+    return this.postJsonObject(baseUrl, jsonObject);
+  }
+
+  updateCalloutTime(jsonObject: JSON): Observable<any> {
+    const baseUrl = 'f01/f01005fn5';
+
+    return this.postJsonObject(baseUrl, jsonObject);
+  }
+
+  updateCalloutYN(jsonObject: JSON):any {
+    const baseUrl = 'f01/f01005fn6';
+
+    return this.saveOrEditMsgJson(baseUrl, jsonObject);
+  }
+
+}
