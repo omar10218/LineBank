f5972665a75060c855d23aa36307f9d6743de128
omar10218 <omar10218@gmail.com>
2021-10-22 11:56:22 +0800

    test

---
+++ src/app/f03013/f03013create/f03013create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-f03013create',
+  templateUrl: './f03013create.component.html',
+  styleUrls: ['./f03013create.component.css']
+})
+export class F03013createComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+
+}
