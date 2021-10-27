a62c7f88d687c8f40c0ab690df0673219126d64f
omar10218 <omar10218@gmail.com>
2021-10-27 09:39:21 +0800

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://my-prod-url',
+  // allowOrigin: 'http://tvdev-eln-web01.lbtwsys:8080/Web'
   allowOrigin: 'http://192.168.0.64:8080/Web'
+  // allowOrigin: 'http://10.244.103.172:8080/Web'
 };
