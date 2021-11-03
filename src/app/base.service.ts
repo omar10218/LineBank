93654ad6990d0b5d5b5a360380624d8dd415872c
omar <omar10218@gmail.com>
2021-11-03 15:12:39 +0800

    身分驗證

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -105,4 +105,54 @@ export class BaseService {
     });
     return await this.getMsgStr(rspCode, rspMsg);
   }
+
+  //true為驗證成功 false為失敗 身分證驗證
+  public checkIdNumberIsValid(id: string): boolean {
+    const regex: RegExp = /^[A-Z][1,2]\d{8}$/
+    if (!regex.test(id)) {
+      return false;
+    } else {
+      const idArray: string[] = id.split('')
+      const intRadix = 10
+      const TAIWAN_ID_LOCALE_CODE_LIST = [
+        1, 10, 19, 28, 37, 46, 55, 64, 39, 73,
+        82, 2, 11, 20, 48, 29, 38, 47, 56, 65,
+        74, 83, 21, 3, 12, 30
+      ]
+
+      const RESIDENT_CERTIFICATE_NUMBER_LIST = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '4', '8',
+        '9', '0', '1', '2', '5', '3', '4', '5', '6', '7',
+        '8', '9', '2', '0', '1', '3'
+      ]
+
+      // if is not a number (居留證編號)
+      if (isNaN(parseInt(idArray[1], intRadix))) {
+        idArray[1] =
+          RESIDENT_CERTIFICATE_NUMBER_LIST[id.charCodeAt(1) - 'A'.charCodeAt(0)]
+      }
+
+      const result = idArray.reduce(
+        (sum: number, n: string, index: number): number => {
+          if (index === 0) {
+            return (
+              sum +
+              TAIWAN_ID_LOCALE_CODE_LIST[
+              idArray[0].charCodeAt(0) - 'A'.charCodeAt(0)
+              ]
+            )
+          } else if (index === 9) {
+            return sum + parseInt(idArray[9], intRadix)
+          }
+          return sum + parseInt(idArray[index], intRadix) * (9 - index)
+        },
+        0
+      )
+
+      if (result % 10 === 0) {
+        return true
+      }
+      return false
+    }
+  }
 }
