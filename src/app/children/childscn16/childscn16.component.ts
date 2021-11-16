3c358f933c9f2a6495bdac5432a817a96c746441
nick-jiangxie <nick.jiangxie@rstn.com.tw>
2021-11-16 13:42:56 +0800

    歷史資料 撈取表單

--- src/app/children/childscn16/childscn16.component.ts
+++ src/app/children/childscn16/childscn16.component.ts
@@ -2,21 +2,18 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChildrenService } from '../children.service';
 import { Childscn16Service } from './childscn16.service';
 import { DatePipe } from '@angular/common';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatDialog } from '@angular/material/dialog';
-import { Childscn1Component } from '../childscn1/childscn1.component'
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { MatSort, Sort } from '@angular/material/sort';
-import { environment } from 'src/environments/environment';
 import { Data } from '@angular/router';
 import { NzTableQueryParams } from 'ng-zorro-antd/table';
 
-//Jay 歷史資料
+//Jay 歷史資料 Nick
 
 @Component({
   selector: 'app-childscn16',
   templateUrl: './childscn16.component.html',
-  styleUrls: ['./childscn16.component.css','../../../assets/css/f03.css']
+  styleUrls: ['./childscn16.component.css', '../../../assets/css/f03.css']
 })
 export class Childscn16Component implements OnInit {
 
@@ -33,75 +30,67 @@ export class Childscn16Component implements OnInit {
   currentPage: PageEvent;
   currentSort: Sort;
   totalCount: any;
-  ruleParamCondition : Data[] = [];
+  ruleParamCondition: Data[] = [];
   transactionLogSource: Data[] = [];
   total = 1;
   loading = false;
   pageSize = 50;
   pageIndex = 1;
-  cuid:string;
+  cuid: string;
   fds: string = "";
 
   ngOnInit(): void {
     this.applno = sessionStorage.getItem('applno');
-    this.cuid= sessionStorage.getItem( 'cuid');
-    console.log(sessionStorage.getItem( 'cuid'))
-  }
-  ngAfterViewInit() {
-    //this.initial( this.pageIndex, this.pageSize );
+    this.cuid = sessionStorage.getItem('cuid');
+    console.log(sessionStorage.getItem('cuid'))
+    this.initial(this.pageIndex, this.pageSize,);
   }
 
-  // ngAfterViewInit() {
-  //   this.currentPage = {
-  //     pageIndex: 0,
-  //     pageSize: 10,
-  //     length: null
-  //   };
-  //   this.currentSort = {
-  //     active: '',
-  //     direction: ''
-  //   };
-
-  // }
   Inquire(id: string, nationalId: string) //查詢
   {
     const url = 'f01/f01002fn1';
     let jsonObject: any = {};
     console.log(this.applno)
-    jsonObject['swcApplno'] =this.applno;
-    this.childscn16Service.selectCustomer(url,jsonObject).subscribe(data =>
-      {
-        console.log(data);
-        // if ( data.rspBody.length > 0 ) {
-        //   this.fds = data.rspBody[0].fds
-        // }
+    jsonObject['swcApplno'] = this.applno;
+    this.childscn16Service.selectCustomer(url, jsonObject).subscribe(data => {
+      console.log(data);
 
-        if (data.rspMsg == '案件鎖定成功')
-        {
-          sessionStorage.setItem( 'applno', id );
-          sessionStorage.setItem( 'cuid', nationalId );
-          sessionStorage.setItem( 'search', 'Y' );
-          sessionStorage.setItem( 'fds', this.fds );
-          sessionStorage.setItem( 'queryDate', '' );
-          //開啟徵審主畫面
-          window.open("http://localhost:4200/#/F01002/F01002SCN1/CHILDSCN1");
-        }
+      if (data.rspMsg == '案件鎖定成功') {
+        sessionStorage.setItem('applno', id);
+        sessionStorage.setItem('cuid', nationalId);
+        sessionStorage.setItem('search', 'Y');
+        sessionStorage.setItem('fds', this.fds);
+        sessionStorage.setItem('queryDate', '');
+        //開啟徵審主畫面
+        window.open("http://localhost:4200/#/F01002/F01002SCN1/CHILDSCN1");
+      }
     })
   }
-  initial(pageIndex: number, pageSize: number)//初始查詢
-  {
-    let url = 'f01/childscn16';
-    this.jsonObject['applno'] = this.applno;
-    this.jsonObject['page'] = pageIndex;
-    this.jsonObject['per_page'] = pageSize;
-    this.childscn16Service.selectCustomer(url, this.jsonObject).subscribe(data => {
-      console.log(data.rspBody.size)
-      this.total = data.rspBody.size;
-      this.ruleParamCondition = data.rspBody.items;
+  //取得表單資料
+  initial(pageIndex: number, pageSize: number) {
+    const baseUrl = 'f01/childbwscn6'
+    let jsonObject: any = {}
+    jsonObject['applno'] = this.applno ;
+    //測試用
+    // jsonObject['applno'] = '1111111';
+    jsonObject['page'] = pageIndex;
+    jsonObject['per_page']= pageSize;
+    console.log('jsonObject');
+    console.log(jsonObject);
+    this.childscn16Service.selectCustomer(baseUrl, jsonObject).subscribe(data => {
+      console.log('data')
+      console.log(data)
+      this.total = data.rspBody.size
+      console.log(this.total)
+      this.ruleParamCondition = data.rspBody.items
     })
+    this.loading = false
   }
+
   onQueryParamsChange(params: NzTableQueryParams): void {
-    const { pageSize, pageIndex } = params;
-    this.initial(pageIndex, pageSize);
+    const { pageSize, pageIndex } = params
+    this.pageSize = pageSize
+    this.pageIndex = pageIndex
+    this.initial(pageIndex, pageSize)
   }
 }
