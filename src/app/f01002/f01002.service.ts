0852ca85a340c74f553cbe6e4b658c9356831027
omar <omar10218@gmail.com>
2021-11-16 14:14:07 +0800

    移動視窗

--- src/app/f01002/f01002.service.ts
+++ src/app/f01002/f01002.service.ts
@@ -1,13 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { BaseService } from '../base.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class F01002Service extends BaseService {
+
   constructor(protected httpClient: HttpClient) { super(httpClient); }
+
   getCaseList(jsonObject: JSON): Observable<any> {
     const baseUrl = 'f01/f01002';
 
