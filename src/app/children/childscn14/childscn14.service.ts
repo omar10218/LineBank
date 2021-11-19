7dded713f92a88facd496fa65e0155946a8ffc09
Mount2020 <76730136+Mount2020@users.noreply.github.com>
2021-11-19 16:34:37 +0800

    微調

--- src/app/children/childscn14/childscn14.service.ts
+++ src/app/children/childscn14/childscn14.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BaseService } from 'src/app/base.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +16,17 @@ export class Childscn14Service extends BaseService {
   // }
 
   //取Table資料
-  getImageInfo(baseUrl: string, applno: string) {
-    let jsonObject: any = {};
-    jsonObject['applno'] = applno;
+  getImageInfo(baseUrl: string, jsonObject: JSON) {
+    // let jsonObject: any = {};
+    // jsonObject['applno'] = applno;
     return this.postJsonObject(baseUrl, jsonObject);
   }
+
+  //上傳圖檔
+  uploadFile(baseUrl: string, fileToUpload: File): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    return this.httpClient.post<any>(environment.allowOrigin + '/' + baseUrl, formData);
+  }
+
 }
