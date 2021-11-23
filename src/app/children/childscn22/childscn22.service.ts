d4c5e0fa5cf30afdd5df54b8bedfaa24152b42fa
a0188218 <a0188218@hotmail.com>
2021-11-23 12:15:40 +0800

    更新

--- src/app/children/childscn22/childscn22.service.ts
+++ src/app/children/childscn22/childscn22.service.ts
@@ -10,7 +10,19 @@ export class Childscn22Service extends BaseService {
   constructor(protected httpClient: HttpClient) { super(httpClient); }
 
   doDss1Search(jsonObject: any): any {
-    const baseUrl = 'f01/childscn22';
+    const baseUrl = 'f01/childscn22action1';
+    let targetUrl = `${baseUrl}`;
+    return this.saveOrEditMsgString(targetUrl, jsonObject);
+  }
+
+  doDss2Search(jsonObject: any): any {
+    const baseUrl = 'f01/childscn22action2';
+    let targetUrl = `${baseUrl}`;
+    return this.saveOrEditMsgString(targetUrl, jsonObject);
+  }
+
+  doDss4Search(jsonObject: any): any {
+    const baseUrl = 'f01/childscn22action3';
     let targetUrl = `${baseUrl}`;
     return this.saveOrEditMsgString(targetUrl, jsonObject);
   }
