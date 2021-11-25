14387d66429d84bf386e04ff82e7be3e228af463
Jay <mingjie0201@gmail.com>
2021-11-25 14:21:38 +0800

    1125 jay

--- src/app/children-bw/childbwscn12/childbwscn12.component.ts
+++ src/app/children-bw/childbwscn12/childbwscn12.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-childbwscn12',
   templateUrl: './childbwscn12.component.html',
-  styleUrls: ['./childbwscn12.component.css']
+  styleUrls: ['./childbwscn12.component.css','../../../assets/css/child.css']
 })
 export class Childbwscn12Component implements OnInit {
 
   constructor() { }
 
+  swcID:string;
   ngOnInit(): void {
+    this.swcID = sessionStorage.getItem('cuid');
   }
 
 }
