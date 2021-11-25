fac2803da3a3474123f40751eeaa3264c3f09d9d
Jay <mingjie0201@gmail.com>
2021-11-25 10:47:14 +0800

    test

---
+++ src/app/children-bw/childbwscn12/childbwscn12.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-childbwscn12',
+  templateUrl: './childbwscn12.component.html',
+  styleUrls: ['./childbwscn12.component.css']
+})
+export class Childbwscn12Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
