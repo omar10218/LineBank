635e3adb48b6fcdbe7316fbe4895aa0099b8b8f2
YungHsing <x70343@gmail.com>
2021-12-01 12:37:23 +0800

    調整環境變數檔

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'http://192.168.0.64:8080/Web'
+  allowOrigin: 'https://tvdev-eln-was01.lbtwsys.com:18443/Web'
 };
