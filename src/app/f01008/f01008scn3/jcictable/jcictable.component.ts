a2f182764c37c3d81edcef6709ed5ff8734ed851
omar <omar10218@gmail.com>
2021-12-03 14:24:34 +0800

    test

---
+++ src/app/f01008/f01008scn3/jcictable/jcictable.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jcictable',
+  templateUrl: './jcictable.component.html',
+  styleUrls: ['./jcictable.component.css']
+})
+export class JcictableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
