2c23d81bf04d29a73c2bd596489d5d64b7dbff07
Jay <mingjie0201@gmail.com>
2021-12-06 10:53:11 +0800

    1206 Jay 半成品

--- src/app/f01008/f01008scn1/f01008scn1.component.ts
+++ src/app/f01008/f01008scn1/f01008scn1.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription } from 'rxjs';
+import { F01008Service } from '../f01008.service';
 @Component({
   selector: 'app-f01008scn1',
   templateUrl: './f01008scn1.component.html',
-  styleUrls: ['./f01008scn1.component.css','../../../assets/css/f01.css']
+  styleUrls: ['./f01008scn1.component.css', '../../../assets/css/f01.css']
 })
 export class F01008scn1Component implements OnInit {
 
-  constructor() { }
-
+  constructor(private f01008Service: F01008Service) {
+    this.JCICAddSource$ = this.f01008Service.JCICAddSource$.subscribe((data) => {
+      this.addData = data;
+      this.isShowAdd = data.show;
+    });
+    this.JCICSource$ = this.f01008Service.JCICSource$.subscribe((data) => {
+      this.editData = data;
+      this.isShowEdit = data.show;
+    });
+  }
+  JCICSource$: Subscription;
+  JCICAddSource$: Subscription;
+  isShowAdd: boolean;
+  isShowEdit: boolean;
+  addData: any;
+  editData: any;
   ngOnInit(): void {
+    console.log(this.addData)
+    console.log("123")
   }
 
   ngAfterViewInit() {
--- src/app/f01008/f01008scn2/f01008scn2.component.ts
+++ src/app/f01008/f01008scn2/f01008scn2.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { F01008addComponent } from '../f01008add/f01008add.component'
 import { F01008Service } from '../f01008.service';
 import { Data } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 interface sysCode {
   value: string;
@@ -18,7 +19,10 @@ export class F01008scn2Component implements OnInit {
 
   constructor(
     public dialog: MatDialog,
-    private f01008Service: F01008Service) { }
+    private f01008Service: F01008Service)
+    {
+
+    }
 
   applno: string;
   page = 1;
@@ -26,10 +30,17 @@ export class F01008scn2Component implements OnInit {
   dataSource: Data[] = [];
   tYPE: sysCode[] = [];
   cONDITION: sysCode[] = [];
+
+
+  empNo: string;//員編
+
+  showAdd: boolean = false;
+  showEdit: boolean = false;
   ngOnInit(): void {
     // this.applno = sessionStorage.getItem('applno');
     this.applno = "20211125A00002";
     console.log(this.applno)
+    this.empNo = localStorage.getItem("empNo");
     this.set();
     this.tYPE.push({value:'1',viewValue:'公司電話'})
     this.tYPE.push({value:'2',viewValue:'手機號碼'})
@@ -42,9 +53,30 @@ export class F01008scn2Component implements OnInit {
     this.cONDITION.push({value:'4',viewValue:'其他(備註)'})
   }
   add() {
-    const dialogRef = this.dialog.open(F01008addComponent, {
+    console.log('我進來了1');
+    this.showAdd = !this.showAdd;
+    this.f01008Service.setJCICAddSource({
+      minHeight: '70vh',
+			width: '50%',
+      show: this.showAdd,
+      applno: this.applno,//案件編號
+      // CON_TYPE_Code: this.CON_TYPE_Code,//聯絡方式下拉選單
+      CON_TYPE: '',//聯絡方式
+      // TEL_CONDITION_Code: this.TEL_CONDITION_Code,//電話狀況下拉選單
+      TEL_CONDITION: '',//電話狀況
+      // TEL_CHECK_Code: this.TEL_CHECK_Code,//電話種類下拉選單
+      TEL_CHECK: '',//電話種類
+      // HOURS_Code: this.HOURS_Code,//時下拉選單
+      HOURS: '',//時種類
+      // MINUTES_Code: this.MINUTES_Code,//分下拉選單
+      MINUTES: '',//分種類
+      PHONE: '',//手機/市話
+      CON_MEMO: '',//備註
+      CALLOUT_DATE: '',//設定下次照會時間
+      CALLOUT_SETTIME: '',//確認時間
+      CALLOUT_EMPNO: this.empNo,//徵信員編
 
-    });
+    })
   }
   set() {
     let jsonObject: any = {};
