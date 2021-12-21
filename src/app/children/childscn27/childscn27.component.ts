6fa78cbe11bf50a97b1effb4bd794b8140d1d097
nick-jiangxie <nick.jiangxie@rstn.com.tw>
2021-12-21 18:11:02 +0800

    發mail 發簡訊

---
+++ src/app/children/childscn27/childscn27.component.ts
@@ -0,0 +1,125 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Childscn27Service } from './childscn27.service';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ConfirmComponent } from 'src/app/common-lib/confirm/confirm.component';
+import { DatePipe } from '@angular/common';
+
+interface sysCode {
+  value: string;
+  viewValue: string;
+}
+
+//Nick 簡訊
+@Component({
+  selector: 'app-childscn27',
+  templateUrl: './childscn27.component.html',
+  styleUrls: ['./childscn27.component.css', '../../../assets/css/f01.css']
+})
+export class Childscn27Component implements OnInit {
+
+  constructor(
+    private childscn27Service: Childscn27Service,
+    public dialog: MatDialog,
+    public dialogRef: MatDialogRef<Childscn27Component>,
+    private pipe: DatePipe,
+  ) { }
+
+  private applno: string;                           //案編
+  mobile: string;                                   //客戶手機
+  smsSetCode: sysCode[] = [];                       //sms模板下拉
+  smsSet: string;                                   //sms模板
+  content: string;                                  //sms內容
+  messageContent: string;                           //由sms模板代入sms內容
+  realSmsTime: string;                              //預計發送時間(日期)
+  realSmsTimeValue: Date;                           //預計發送時間類型
+  mytime: Date | null = null;                       //預計發送時間(時分)
+  smsDataSource = new MatTableDataSource<any>();    //簡訊資訊檔
+  sms_M_Code = new MatTableDataSource<any>();    //sms mappingcode
+
+
+  ngOnInit(): void {
+    //取案編
+    this.applno = sessionStorage.getItem('applno');
+    //取sms樣板下拉
+    this.childscn27Service.getSysTypeCode('SMS_SET').subscribe(data => {
+      for (const jsonObj of data.rspBody.mappingList) {
+        const codeNo = jsonObj.codeNo;
+        const desc = jsonObj.codeDesc;
+        this.smsSetCode.push({ value: codeNo, viewValue: desc })
+      }
+      this.sms_M_Code.data = data.rspBody.mappingList;
+    });
+    this.getSmsList();
+  }
+
+  //發送簡訊
+  async addSms() {
+    this.messageContent = this.content;
+    if (this.realSmsTime == null) {
+      const confirmDialogRef = this.dialog.open(ConfirmComponent, {
+        data: { msgStr: "請輸入日期" }
+      });
+    } else if (this.realSmsTime != null && this.mytime == null) {
+      const confirmDialogRef = this.dialog.open(ConfirmComponent, {
+        data: { msgStr: "請輸入時間" }
+      });
+    } else if (this.content == null) {
+      const confirmDialogRef = this.dialog.open(ConfirmComponent, {
+        data: { msgStr: "請輸入SMS內容" }
+      });
+    } else if (this.content != null) {
+      if (this.content.indexOf('徵審人員修改') >= 0) {
+        const confirmDialogRef = this.dialog.open(ConfirmComponent, {
+          data: { msgStr: "不得有徵審人員修改字樣" }
+        });
+      }
+      else {
+        let msgStr: string = "";
+        const baseUrl = 'f01/childscn27action1';
+        let jsonObject: any = {};
+        jsonObject['applno'] = this.applno;
+        jsonObject['messageContent'] = this.messageContent;
+        jsonObject['empno'] = localStorage.getItem("empNo");
+        jsonObject['mobile'] = this.mobile;
+        jsonObject['realSmsTime'] = this.pipe.transform(this.realSmsTime, 'yyyyMMdd') + this.pipe.transform(this.mytime, 'HHmm');
+        await this.childscn27Service.postJson(baseUrl, jsonObject).subscribe(data => {
+          msgStr = data.rspMsg == "success" ? "新增成功!" : ""
+          const childernDialogRef = this.dialog.open(ConfirmComponent, {
+            data: { msgStr: msgStr }
+          });
+          if (data.rspMsg == "success" && data.rspCode === '0000') {
+            this.getSmsList();
+          }
+        });
+      }
+    }
+  }
+
+  // 選取sms模板後會將內容代入sms內容
+  changeSelect(smsSet: string) {
+    for (const jsonObj of this.sms_M_Code.data) {
+      this.content = jsonObj.codeNo == smsSet ? jsonObj.codeTag : this.content;
+    }
+    // this.childscn27Service.getSmsContent(smsSet).subscribe(data => {
+    //   this.content = data.rspBody[0].codeTag;
+    // })
+  };
+
+  //取該案件簡訊發送資訊/從客戶資訊查詢客戶手機
+  getSmsList() {
+    const baseUrl = 'f01/childscn27';
+    let jsonObject: any = {};
+    jsonObject['applno'] = this.applno;
+    this.childscn27Service.postJson(baseUrl, jsonObject).subscribe(data => {
+      this.smsDataSource = data.rspBody.items;
+      this.mobile = data.rspBody.phone;
+    });
+  };
+
+  // 離開該彈窗
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
+}
