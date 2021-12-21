f32f0359bd5740ea4519b70450d3d04781040593
Mount2020 <76730136+Mount2020@users.noreply.github.com>
2021-12-21 12:04:27 +0800

    20211221

--- src/app/children/childscn14/childscn14page3/childscn14page3.component.ts
+++ src/app/children/childscn14/childscn14page3/childscn14page3.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Childscn1Service } from '../../childscn1/childscn1.service';
+import { Childscn6Service } from '../../childscn6/childscn6.service';
 import { Childscn14Service } from '../childscn14.service';
 
 interface sysCode {
@@ -21,23 +22,37 @@ export class Childscn14page3Component implements OnInit {
     public dialogRef: MatDialogRef<Childscn14page3Component>,
     private fb: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: any,
+    private childscn6Service: Childscn6Service,
     private childscn14Service: Childscn14Service,
     private childscn1Service: Childscn1Service,
     ) { }
-  fileToUpload: File | null = null;
+  // fileToUpload: File | null = null;
+  public fileToUpload: any;
   private applno: string;
   private cuid: string;
+  private cuNm: string;
   isValidFile: boolean;
 
   ngOnInit(): void {
 
-    // this.applno = sessionStorage.getItem('applno');
-    // this.cuid = sessionStorage.getItem('cuid');
+    this.applno = sessionStorage.getItem('applno');
+    this.cuid = sessionStorage.getItem('cuid');
     // const baseUrl = 'f01/childscn14action1';
     // let jsonObject: any = {};
     // jsonObject['applno'] = this.applno;
     // this.childscn14Service.getImageInfo(baseUrl, jsonObject).subscribe(data => {
     // });
+    // this.applno = sessionStorage.getItem('applno');
+    // this.search = sessionStorage.getItem('search');
+    // this.host = this.getHost();
+
+    const baseUrl = 'f01/childscn6action2';
+    let jsonObject: any = {};
+    this.childscn6Service.getDate(baseUrl, jsonObject).subscribe(data => {
+      this.cuid = data.rspBody[0].empNo;
+      this.cuNm = data.rspBody[0].empName;
+    });
+
 
     this.childscn1Service.getSysTypeCode('DOC_TYPE').subscribe(data => {        //文件類型下拉選單
         for (const jsonObj of data.rspBody.mappingList) {
@@ -57,18 +72,19 @@ export class Childscn14page3Component implements OnInit {
   public async upload(): Promise<void> {
     let docTypeCode = this.uploadForm.value.DOC_TYPE_CODE;
     if (docTypeCode != "" && docTypeCode != null) {
-      const formdata: FormData = new FormData();
+      const formdata = new FormData();
+
       formdata.append('file', this.fileToUpload, this.fileToUpload.name);
       formdata.append('applno', this.applno);
       formdata.append('cuId', this.cuid);
+      formdata.append('cuNm', this.cuNm);
       formdata.append('docTypeCode', this.uploadForm.value.DOC_TYPE_CODE);
       formdata.append('remark', this.uploadForm.value.REMARK);
       let baseUrl = 'f01/childscn14action2';
-
-      this.childscn14Service.childscn14Action(baseUrl, formdata).then((data: any) => {
+        await this.childscn14Service.childscn14Action(baseUrl, formdata).then((data: any) => {
         if (data.rspMsg != '文件上傳成功') {
         this.uploadForm.patchValue({ ERROR_MESSAGE: data.rspMsg });
-        alert(data.rspMsg);
+        alert('文件上傳失敗，請聯絡系統管理人員: ' + data.rspMsg);
         } else {
           alert(data.rspMsg);
           this.dialogRef.close();
