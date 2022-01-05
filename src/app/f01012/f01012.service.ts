763ebb9afcee5ca76b388f7147c25a897cfd1ef2
Jay <jay.lee@rstn.com.tw>
2022-01-05 10:15:01 +0800

    0105 jay 合約前覆核清單

---
+++ src/app/f01012/f01012.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { BaseService } from '../base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class F01012Service  extends BaseService {
+
+  constructor(protected httpClient: HttpClient) { super(httpClient); }
+
+
+
+}
