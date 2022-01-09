7d94791b67078ee2938a29b8aeccabe608d1be8d
YungHsing <x70343@gmail.com>
2022-01-09 16:18:40 +0800

    調整SSO驗證架構 調整SSO驗證架構 調整SSO驗證架構

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'http://localhost:8080/Web'
+  allowOrigin: 'http://localhost:8080/Web',
+  from: 'local'
 };
