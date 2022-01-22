0deea2cae3a579716470564bab727a8f91e53f54
a0188218 <a0188218@hotmail.com>
2022-01-22 16:04:19 +0800

    更新

--- src/app/f01015/f01015.component.ts
+++ src/app/f01015/f01015.component.ts
@@ -4,10 +4,11 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { ConfirmComponent } from '../common-lib/confirm/confirm.component';
+import { OptionsCode } from '../interface/base';
 import { F01015Service } from './f01015.service';
 
 interface sysCode {
-  viewValue: string
+  viewValue: string;
 }
 @Component({
   selector: 'app-f01015',
@@ -15,23 +16,32 @@ interface sysCode {
   styleUrls: ['./f01015.component.css', '../../assets/css/f01.css']
 })
 export class F01015Component implements OnInit {
-  nationalId: string //身分證字號
-  custId: string //customer_ID
-  targetCustSource = [] //解凍降額Table
-  creditMainSource = [] //貸後管理紀錄Table
+  nationalId: string; //身分證字號
+  custId: string; //customer_ID
+  targetCustSource = []; //解凍降額Table
+  creditMainSource = []; //貸後管理紀錄Table
   total = 1;
   loading = true;
   pageSize = 10;
   pageIndex = 1;
+  YNCode: OptionsCode[] = [];
+  YNValue: string;
 
   constructor(
-    private F01015Service: F01015Service,
+    private f01015Service: F01015Service,
     public dialog: MatDialog,
-    private datePipe: DatePipe,
-
+    private datePipe: DatePipe
   ) { }
 
   ngOnInit(): void {
+    this.f01015Service.getSysTypeCode('YN').subscribe(data => {
+      this.YNCode.push({ value: '', viewValue: '請選擇' })
+      for (const jsonObj of data.rspBody.mappingList) {
+        const codeNo = jsonObj.codeNo;
+        const desc = jsonObj.codeDesc;
+        this.YNCode.push({ value: codeNo, viewValue: desc })
+      }
+    });
   }
   getTargetCustList() {
     if ((this.nationalId == null || this.nationalId == '') && (this.custId == null || this.custId == '')) {
@@ -42,7 +52,7 @@ export class F01015Component implements OnInit {
       let jsonObject: any = {};
       jsonObject['nationalId'] = this.nationalId
       jsonObject['custId'] = this.custId
-      this.F01015Service.getImpertmentParameter(jsonObject).subscribe(data =>{
+      this.f01015Service.getImpertmentParameter(jsonObject).subscribe(data =>{
         console.log(data)
         this.targetCustSource=data.rspBody.items
         this.creditMainSource=data.rspBody.creditMainlist
