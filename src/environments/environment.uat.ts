61419fcc56265733f68bcf731a299b3dccc89eb7
YungHsing <x70343@gmail.com>
2022-01-24 16:38:48 +0800

    uat url

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: true,
   apiUrl: 'http://my-prod-url',
-  allowOrigin: 'https://lb-tvuat-eln-was-18443.lbtwsys.com:18443/Web',
+  allowOrigin: 'https://tvuat-eln-was01.lbtwsys.com:18443/Web',
   from: 'uat'
 };
+
+// 'https://lb-tvuat-eln-was-18443.lbtwsys.com:18443/Web'
