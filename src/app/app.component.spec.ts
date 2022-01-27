709850beda2a4e880c95d4c9b0e78eeb873d11ea
Jay <jay.lee@rstn.com.tw>
2022-01-27 15:47:21 +0800

    0127 jay

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RoutingPracticeForAngular30Days'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('RoutingPracticeForAngular30Days');
-  });
+  // it(`should have as title 'RoutingPracticeForAngular30Days'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('RoutingPracticeForAngular30Days');
+  // });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('RoutingPracticeForAngular30Days app is running!');
-  });
+  // it('should render title', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.nativeElement;
+  //   expect(compiled.querySelector('.content span').textContent).toContain('RoutingPracticeForAngular30Days app is running!');
+  // });
 });
