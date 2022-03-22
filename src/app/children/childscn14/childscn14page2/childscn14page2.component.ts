9f3ca39082cb22aa224d21ca316c60f68b26b1cb
Mount2020 <76730136+Mount2020@users.noreply.github.com>
2022-03-22 11:00:18 +0800

    20220322_移除不必要程式

--- src/app/children/childscn14/childscn14page2/childscn14page2.component.ts
+++ src/app/children/childscn14/childscn14page2/childscn14page2.component.ts
@@ -22,34 +22,11 @@ export class Childscn14page2Component implements OnInit {
     public childService: ChildrenService
   ) { }
 
-  imageForm: FormGroup = this.fb.group({
-    APPLNO: ['', []]
-  });
-  dateCode: dateCode[] = [];
-  dateValue: string;
-
-
   private applno: string;
   private cuid: string;
 
   ngOnInit(): void {
-    this.applno = sessionStorage.getItem('applno');
-    this.cuid = sessionStorage.getItem('nationalId');
 
-    const url = 'f01/childscn14';
-    const formdata: FormData = new FormData();
-    formdata.append('applno', this.applno);
-    formdata.append('cuid', this.cuid);
-    this.imageForm.patchValue({ APPLNO: this.applno })
   }
 
-  getApplno(): String {
-    return this.applno;
-  }
-
-  getHost(): String {
-    var origin = window.location.origin;
-    var host = origin.substring(0, origin.lastIndexOf(":"));
-    return host;
-  }
 }
