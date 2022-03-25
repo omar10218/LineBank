316b51d0dea5fd59a0f80098065ed887983aebb2
omar <omar10218@gmail.com>
2022-03-25 12:02:51 +0800

    20220325 取案件原編方式修正

--- src/app/f01003/f01003.component.ts
+++ src/app/f01003/f01003.component.ts
@@ -92,7 +92,7 @@ export class F01003Component implements OnInit, AfterViewInit {
     let jsonObject: any = {};
     jsonObject['page'] = this.pageIndex;
     jsonObject['per_page'] = this.pageSize;
-    jsonObject['swcL2EmpNo'] = this.empNo;
+    jsonObject['swcL2EmpNo'] = BaseService.userId;
     jsonObject['swcNationalId'] = this.swcNationalId;
     jsonObject['swcCustId'] = this.swcCustId;
     jsonObject['swcApplno'] = this.swcApplno;
