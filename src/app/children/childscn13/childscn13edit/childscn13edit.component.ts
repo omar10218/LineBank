672c0abda8e74d5264dc4a556cc71895b8e38748
omar <omar10218@gmail.com>
2022-03-28 12:34:37 +0800

    ELOAN-JIRA單號:818

--- src/app/children/childscn13/childscn13edit/childscn13edit.component.ts
+++ src/app/children/childscn13/childscn13edit/childscn13edit.component.ts
@@ -56,7 +56,7 @@ export class Childscn13editComponent implements OnInit {
   onAddImage(event: any) {
     if (this.files != null) {
       var mimeType = this.files.type;
-      if (mimeType.match(/image\/*/) == null) {
+      if (mimeType.match(/(.jpg|.jpeg|.png|.JPG|.JPEG|.PNG)/) == null) {
         this.msg = "檔案非圖片類型!";
         this.imageSrc = '';
       } else {
